c9fe66d586298ae7f3243679ec8d0dbf703adef3
Louis Vialar <louis.vialar@gmail.com>
2023-06-12 23:16:48 +0200

    Make autocrafter use a config

--- autocrafter/autocrafter.lua
+++ autocrafter/autocrafter.lua
@@ -1,25 +1,7 @@
 -- pastebin get 4n0u6M3F startup
 
 rs_controller_side = "bottom"
-
-containers = {
-	-- Items on the sides of the computer
-	["peripheral"] = {
-		["back"] = {
-			"bigreactors:yellorite_ore",
-			"minecraft:raw_iron",
-			"mekanism:raw_osmium",
-			"minecraft:raw_gold",
-			"mekanism:raw_tin",
-			"create:raw_zinc",
-			"minecraft:raw_copper",
-			"immersiveengineering:raw_nickel",
-			"occultism:raw_silver"
-		}
-	},
-	-- Items on the side of the RS controller
-	["side"] = {}
-}
+containers = require("config")
 
 --
 
---
+++ autocrafter/config.lua
@@ -0,0 +1,18 @@
+{
+	-- Items on the sides of the computer
+	["peripheral"] = {
+		["back"] = {
+			"bigreactors:yellorite_ore",
+			"minecraft:raw_iron",
+			"mekanism:raw_osmium",
+			"minecraft:raw_gold",
+			"mekanism:raw_tin",
+			"create:raw_zinc",
+			"minecraft:raw_copper",
+			"immersiveengineering:raw_nickel",
+			"occultism:raw_silver"
+		}
+	},
+	-- Items on the side of the RS controller
+	["side"] = {}
+}
